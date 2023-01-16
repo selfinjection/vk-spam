2c4e95d291fd43f02ad3964350913251d84a6a9e
trace <yoyort21@protonmail.com>
2023-01-16 06:23:06 +0000

    check_links optimization

--- src/utils.py
+++ src/utils.py
@@ -1,16 +1,23 @@
 import requests
 from lxml.html import fromstring
 from loguru import logger
+import concurrent.futures
+import time
 
-def check_links(links):
+
+def check_links(links, threads=1):
+    invalid_titles = ['Post deleted | VK', 'Запись удалена', 'Error | VK']
     result = []
+    responses = []
     for link in links:
-        invalid_titles = ['Post deleted | VK', 'Запись удалена', 'Error | VK']
-        response = requests.get(link)
-        response = fromstring(response.content)
-        if response.findtext('.//title') in invalid_titles:
-            logger.debug(f'{link} post DELETED')
+        responses.append(requests.get(link))
+        time.sleep(0.5)
+    
+    for response in responses:
+        html = fromstring(response.content)
+        if html.findtext('.//title') in invalid_titles:
+            logger.debug(f'{response.url} post DELETED')
             continue
-        logger.debug(f'{link} post VALID')
-        result.append(link)
+        logger.debug(f'{response.url} post VALID')
+        result.append(response.url)
     return result
